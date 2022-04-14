5396c8cfa7527b4a087a7efe71f20ab7d8ef5a3d
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-04-14 17:08:46 +0900

    deleted auth categories.

--- amplify/backend/function/nomocaliffappeb62764dPreSignup/src/custom.js
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = function (event, context) {
-  if (event.triggerSource === "PreSignUp_SignUp") {
-    event.response.autoConfirmUser = true;
-  }
-  return event;
-};
