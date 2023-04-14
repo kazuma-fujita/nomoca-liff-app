2bcdb6fcb3a487e579a9f67a8b612853d760f157
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2023-04-14 19:24:18 +0900

    debug console.log

--- src/hooks/use-fetch-user.tsx
+++ src/hooks/use-fetch-user.tsx
@@ -156,6 +156,7 @@ const fetchPatient = async (): Promise<Patient | null> => {
   }
 
   const patients = result.data.listPatients.items as Patient[];
+  console.log("patients", patients);
   // LINEユーザー:cognitoユーザー:patientデータは 1:1:1 なので複数patientデータはエラー処理
   //   if (patients.length > 1) {
   //     throw Error("It was found two patients or over.");
